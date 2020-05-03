6e07e2ad544bef79b010c853f89c66fb843dcbd1
Thanh Binh <lethanhbinh3920@gmail.com>
2020-05-03 13:35:52 +0700

    Feat: setup info change pass and test user

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket
 import os
 import sign
 import pickle #to send object
+import re
 from getpass import getpass
 
 host = input("Input IP server: ")
@@ -21,9 +22,61 @@ def log():
         s.send(msg)
     else:
         user = sign.regis(user)
-    
+        s.send(bytes("regis", "utf-8"))
+        msg = pickle.dumps(user)
+        s.send(msg)
+    msg = s.recv(numByteReceive)
+    if msg.decode("utf-8") == "success":
+        return True
+    else:
+        print("Something went wrong!!")
+        tempInput = input("Press anykey to try again")
+        return log()
+
+def changePass():
+    global user
+    newPass = sign.changePass(user)
+    if newPass == False:
+        return False
+    s.send(bytes("newpass", "utf-8"))
+    msg = pickle.dumps(user)
+    s.send(msg)
+
+def checkUser(username, option):
+    s.send(bytes("check"),"utf-8")
+    s.send(bytes(username),"utf-8")
+    s.send(bytes(option),"utf-8")
+    msg = s.recv(numByteReceive)
+    print(msg.decode("utf-8"))
+
+def setupInfo(option):
+    global user
+    s.send(bytes("update"),"utf-8")
+    s.send(bytes(user["username"]),"utf-8")
+    s.send(bytes(option),"utf-8")
+    msg = s.recv(numByteReceive)
+
+def analyzeCommand(command):
+    splitCmd = re.split("\s", command)
+    if splitCmd[0] == "change_password":
+        changePass()
+    elif splitCmd[0] == "check_user":
+        checkUser(splitCmd[2], splitCmd[1])
+    elif splitCmd[0] == "setup_info":
+        setupInfo(splitCmd[1])
+    else:
+
+
+        
+        return 0
+
 def main():
-    log()
+    if log() == True:
+        print("You have successfully connected to the server!!!!")
+    
+    while(True):
+        command = input()
+
 
 main()
 s.close()
--- sign.py
+++ sign.py
@@ -10,7 +10,7 @@ def sign():
 def login(user):
     user["username"] = input("Please enter your username: ")
     user["password"] = getpass("Please enter your password: ")
-    encrypt(user)
+    user = encrypt(user)
     return user
 
 def regis(user):
@@ -18,7 +18,7 @@ def regis(user):
     user["password"] = getpass("Please enter your password: ")
     user["fullname"] = input("Enter your full name: ")
     user["birth"] = input("Enter your birthday")
-    encrypt(user)
+    user = encrypt(user)
     return user
 
 
@@ -29,7 +29,17 @@ def encrypt(user):
         #add code below this line
             #encrypt password
         #add code above this line
-        return 1
+        return user
     else:
         print("You choose not to encrypt your password.")
-        return 0
+        return user
+
+def changePassword(user):
+    oldPass = getpass("password: ")
+    if oldPass == user["password"]:
+        newPass = getpass("new password: ")
+        user["password"] = newPass
+        user = encrypt(user)
+        return user["password"]
+    else:
+        return False
